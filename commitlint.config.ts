783812e8eb8c23957a061b15cbf8388e738e217d
outsourcing001 <outsourcing001@IT_OUTSOURCING4.cathaysite.com.tw>
2024-03-22 16:50:49 +0800

    core: add commitlint for linting commit message

---
+++ commitlint.config.ts
@@ -0,0 +1,29 @@
+import type {UserConfig} from '@commitlint/types'; 
+import {RuleConfigSeverity} from '@commitlint/types'; 
+
+const CommitLintConfigurations: UserConfig = {
+  extends: ['@commitlint/config-conventional'], // lint 的基底檔案為 @commitlint/config-conventional 裡的規則
+  rules: {
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'build',
+        'core',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
+
+}
+
+export default CommitLintConfigurations;
